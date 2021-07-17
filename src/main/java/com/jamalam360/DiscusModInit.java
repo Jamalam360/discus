79469965dd11a597824fadc6cc2dec4470a1f88f
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-07-17 12:10:52 +0100

    init, init?

--- src/main/java/com/jamalam360/DiscusModInit.java
+++ src/main/java/com/jamalam360/DiscusModInit.java
@@ -30,7 +30,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class TemplateModInit implements ModInitializer {
+public class DiscusModInit implements ModInitializer {
     public static Logger LOGGER = LogManager.getLogger();
 
     public static final String MOD_ID = "discus";
