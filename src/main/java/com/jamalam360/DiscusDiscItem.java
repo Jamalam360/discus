6539dbb269a5b066a7e5910441fc0350e9920abc
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-07-17 21:15:10 +0100

    Test commit

--- src/main/java/com/jamalam360/DiscusDiscItem.java
+++ src/main/java/com/jamalam360/DiscusDiscItem.java
@@ -6,6 +6,6 @@ import net.minecraft.sound.SoundEvent;
 //To get around mojank MusicDiscItem being protected
 public class DiscusDiscItem extends MusicDiscItem {
     public DiscusDiscItem(int comparatorOutput, SoundEvent sound, Settings settings) {
-        super(comparatorOutput, sound, settings);
+        super(comparatorOutput, sound, settings)
     }
 }
